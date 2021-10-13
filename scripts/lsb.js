ebf0e1daba4663500409913b34b566c946153d43
Joel Retkowski <joelretkowski@gmail.com>
2021-10-13 09:56:52 -0700

    Testing add of new oauth page

--- scripts/lsb.js
+++ scripts/lsb.js
@@ -14,6 +14,111 @@ Vue.use(window.VueTimeago, {
 //
 // Mixins
 
+var OAuthRequest = {
+    data: function () {
+        return {
+            loading: false,
+            stateTimer: "",
+            authState: "",
+            authCode: "",
+            authWin: null,
+            clientId: "2i9b9y2f52u4g3v3vyova9xbey8nb2",
+            redirectUrl: "https://luckyscuffbot.github.io/auth",
+            scopes: "channel:read:subscriptions%20channel:read:redemptions%20user:read:subscriptions",
+            scopeDescriptions: {
+                "channel:read:subscriptions": "Get a list of all subscribers to your channel and check if a user is subscribed to your channel.",
+                "channel:read:redemptions": "View your channel points custom reward redemptions.",
+                "user:read:subscriptions": "Get the details of your subscription to a channel."
+            }
+        }
+    },
+    computed: {
+        twitchAuthUrl: function () {
+            return "https://id.twitch.tv/oauth2/authorize?client_id=" + this.clientId + "&redirect_uri=" + this.redirectUrl + "&response_type=code&scope=" + this.scopes + "&state=" + this.authState
+        },
+        appScopes: function () {
+            let appScopes = [{ Type: "Chat", Category: "Messaging", Action: "Login", Description: "Ability to log into chat and send messages." }];
+
+            let scopes = this.scopes;
+            let allScopes = scopes.split("%20");
+
+            for (let i = 0; i < allScopes.length; i++) {
+                let s = allScopes[i];
+                let description = this.scopeDescriptions[s];
+                let scopeSplit = s.split(":");
+
+                if (scopeSplit.length < 3)
+                    continue;
+
+                appScopes.push({ Type: scopeSplit[0], Action: scopeSplit[1], Category: scopeSplit[2], Description: description });
+            }
+
+            return appScopes;
+        }
+    },
+    created: function () {
+        // Check initial state
+        this.checkState();
+
+        // Every minute check and revaluate state
+        setInterval(this.checkState, 60000);
+    },
+    mounted: function () {
+        let uri = window.location.search.substring(1);
+        let params = new URLSearchParams(uri);
+        if (params.get("code") && params.get("state")) {
+            let state = null;
+            let storedState = localStorage.getItem("lsb_state");
+            if (storedState) {
+                state = JSON.parse(storedState);
+            }
+            if (state) {
+                thise.authCode = state.authCode;
+            }
+            debugger;
+        } else {
+
+        }
+        this.loading = false;
+    },
+    methods: {
+        startAuth: function () {
+            let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=735,height=800,left=-1000,top=-1000`;
+            let authWindow = window.open(this.twitchAuthUrl, "Twitch Authorize", params);
+
+            authWindow.addEventListener('load', function () {
+                debugger;
+            }, false);
+        },
+        checkState: function () {
+            let state = null;
+            let stateThreshold = 1 * 60 * 1000;
+            let storedState = localStorage.getItem("lsb_state");
+
+            if (storedState == null) {
+                localStorage.setItem("lsb_state", JSON.stringify(this.generateUniqueState()));
+            } else {
+                state = JSON.parse(storedState);
+
+                // Check for stale state
+                if ((new Date() - new Date(state.timestamp)) >= stateThreshold) {
+                    state = this.generateUniqueState();
+                    localStorage.setItem("lsb_state", JSON.stringify(state));
+                }
+            }
+            this.authState = state.key;
+        },
+        generateUniqueState: function () {
+            let state = {
+                "key": Math.random().toString(36).substr(2, 9),
+                "timestamp": new Date()
+            };
+            console.log(state);
+            return state;
+        }
+    }
+};
+
 // Raffle host mixin
 var RaffleHost = {
     data: function () {
